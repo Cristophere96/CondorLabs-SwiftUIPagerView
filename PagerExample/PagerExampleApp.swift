949b690549b924ba52c8441ae4791409af667040
Cristopher Escorcia <cescorcia@condorlabs.io>
2022-03-25 16:44:30 -0500

    Initial Commit

---
+++ PagerExample/PagerExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PagerExampleApp.swift
+//  PagerExample
+//
+//  Created by Cristopher Escorcia on 25/03/22.
+//
+
+import SwiftUI
+
+@main
+struct PagerExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
